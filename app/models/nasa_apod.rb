03a591658960780b71ee85319204e50edbf76f1a
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-05-30 19:45:59 -0400

    debug api not getting fresh data

--- app/models/nasa_apod.rb
+++ app/models/nasa_apod.rb
@@ -1,6 +1,7 @@
 class NasaApod < ActiveRecord::Base
-	def self.get_api_info(date)
-		nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}&date=#{date}"
+	def self.get_api_info(set_date)
+		nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}&date=#{set_date}"
+		Rails.logger.debug("fetching set_date: #{set_date}")
 		request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
 		JSON.parse request_to_nasa_api
 	end
