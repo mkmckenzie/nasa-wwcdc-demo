a92583ab1db571f637af85cf06e5886b4dc5546e
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-05-30 20:21:48 -0400

    set api key via heroku

--- app/models/nasa_apod.rb
+++ app/models/nasa_apod.rb
@@ -1,9 +1,9 @@
 class NasaApod < ActiveRecord::Base
 	def self.get_api_info(set_date)
 		nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}&date=#{set_date}"
-		Rails.logger.debug("fetching set_date: #{set_date}")
 		request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
-		JSON.parse request_to_nasa_api
 		Rails.logger.debug("#{JSON.parse request_to_nasa_api}")
+		JSON.parse request_to_nasa_api
+
 	end
 end
