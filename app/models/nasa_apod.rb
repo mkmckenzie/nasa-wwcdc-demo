72f26a8a52b83d192a57f6dce411fb880861e08d
Mary Katherine McKenzie <mkmckenzie91@gmail.com>
2016-05-30 20:15:38 -0400

    continue to debug heroku not hitting api

--- app/models/nasa_apod.rb
+++ app/models/nasa_apod.rb
@@ -4,5 +4,6 @@ class NasaApod < ActiveRecord::Base
 		Rails.logger.debug("fetching set_date: #{set_date}")
 		request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
 		JSON.parse request_to_nasa_api
+		Rails.logger.debug("#{JSON.parse request_to_nasa_api}")
 	end
 end
